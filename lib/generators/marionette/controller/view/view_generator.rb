d735135087f610f459728b57838658acf367eb9d
Russkikh Artem <rusartx@gmail.com>
2015-02-25 07:14:54 +0500

    controller generator

--- lib/generators/marionette/controller/view/view_generator.rb
+++ lib/generators/marionette/controller/view/view_generator.rb
@@ -4,21 +4,24 @@ require 'generators/marionette/resource_helpers'
 #
 # Example:
 #
-#   rails g marionette:controller root
-#   rails g marionette:controller root index show
-class Marionette::ControllerGenerator < Rails::Generators::Base
+#   rails g marionette:controller:view root
+#   rails g marionette:controller:view root index
+class Marionette::Controller::ViewGenerator < Rails::Generators::Base
   include Marionette::ResourceHelpers
-  desc 'Creates controller'
+  desc 'Creates controller with itemviews'
 
   source_root File.expand_path('../templates', __FILE__)
 
   argument :title, type: :string, banner: "title",
                              desc: "Controller title"
   argument :actions, type: :array, banner: "ACTION ACTION", default: ['index', 'new', 'show', 'edit'],
-                             desc: "Actions for the resource controller"
+                             desc: "Actions for the resource controller and generated views"
 
   def generate_view
     template 'app/controllers/controller.js.coffee',
              "#{javascript_path}/backbone/app/controllers/#{@title.underscore}_controller.js.coffee"
+    @actions.each do |act|
+      generate 'marionette:view', 'item_view', act
+    end
   end
 end
