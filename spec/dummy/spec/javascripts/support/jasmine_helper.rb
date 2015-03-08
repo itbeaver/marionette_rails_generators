edc985983d7b7982ce420e709714f38534a39499
Russkikh Artem <rusartx@gmail.com>
2015-03-09 03:51:09 +0500

    entity spec

--- spec/dummy/spec/javascripts/support/jasmine_helper.rb
+++ spec/dummy/spec/javascripts/support/jasmine_helper.rb
@@ -22,24 +22,28 @@ end
 
 puts 'To skip installation pass rake jasmine PASS_INSTALL=true'
 
-unless ENV["PASS_INSTALL"]
+unless ENV['PASS_INSTALL']
   system('bundle exec rails d marionette:install')
   system('bundle exec rails g marionette:install')
 
-  system("rails g marionette:view TestLayout1 Layout")
-  system("rails g marionette:view TestLayout2 Layout")
-  system("rails g marionette:view TestLayout3 Layout")
+  system('rails g marionette:view TestLayout1 Layout')
+  system('rails g marionette:view TestLayout2 Layout')
+  system('rails g marionette:view TestLayout3 Layout')
 
-  system("rails g marionette:view TestItemView1 ItemView title:string description:text url:text phone:string quantity:integer float_number:float decimal_number:decimal full:boolean email:string password:string")
-  system("rails g marionette:view TestItemView2 ItemView")
-  system("rails g marionette:view test_module/TestItemView3 ItemView")
-  system("rails g marionette:view TestSubsubmodule/test_submodule/TestModule/testItemView4 ItemView")
+  system('rails g marionette:view TestItemView1 ItemView title:string description:text url:text phone:string quantity:integer float_number:float decimal_number:decimal full:boolean email:string password:string')
+  system('rails g marionette:view TestItemView2 ItemView')
+  system('rails g marionette:view test_module/TestItemView3 ItemView')
+  system('rails g marionette:view TestSubsubmodule/test_submodule/TestModule/testItemView4 ItemView')
 
-  system("rails g marionette:view TestCollectionView1 CollectionView")
-  system("rails g marionette:view admin/TestCollectionView2 CollectionView")
-  system("rails g marionette:view TestSubsubmodule/test_submodule/TestModule/TestCollectionView3 CollectionView")
+  system('rails g marionette:view TestCollectionView1 CollectionView')
+  system('rails g marionette:view admin/TestCollectionView2 CollectionView')
+  system('rails g marionette:view TestSubsubmodule/test_submodule/TestModule/TestCollectionView3 CollectionView')
 
-  system("rails g marionette:view TestCompositeView1 CompositeView")
-  system("rails g marionette:view test_module/TestCompositeView2 CompositeView")
-  system("rails g marionette:view TestSubsubmodule/test_submodule/TestModule/testCompositeView3 CompositeView")
+  system('rails g marionette:view TestCompositeView1 CompositeView')
+  system('rails g marionette:view test_module/TestCompositeView2 CompositeView')
+  system('rails g marionette:view TestSubsubmodule/test_submodule/TestModule/testCompositeView3 CompositeView')
+
+  system('rails g marionette:model Posts title:string description:text')
+  system('rails g marionette:model admin/books')
+  system('rails g marionette:model TestSubsubmodule/test_submodule/TestModule/stars')
 end
