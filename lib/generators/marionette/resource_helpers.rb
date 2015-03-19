d73dc5d7ddd11cd00ddbb828f9e3d567a14c1b13
Russkikh Artem <rusartx@gmail.com>
2015-03-20 00:59:18 +0500

    marionette route helper

--- lib/generators/marionette/resource_helpers.rb
+++ lib/generators/marionette/resource_helpers.rb
@@ -41,5 +41,57 @@ module Marionette
     def replace_underscore(string)
       string[0] == '_' ? string[1..-1] : string
     end
+
+    def marionette_route(routing_code, type, modul='All')
+      log :marionette_route, routing_code
+      sentinel = /appRoutes\:\s*$/
+
+      route = routing_code
+
+      case type
+      when :index
+        route = "    \"#{routing_code}(/)\": \"#{routing_code}_index\""
+      when :new
+        route = "    \"#{routing_code}/new(/)\": \"#{routing_code}_new\""
+      when :show
+        route = "    \"#{routing_code}/:id(/)\": \"#{routing_code}_show\""
+      when :edit
+        route = "    \"#{routing_code}/:id/edit(/)\": \"#{routing_code}_edit\""
+      end
+
+      in_root do
+        inject_into_file "#{backbone_path}/routes.js.coffee", "\n  #{route}", { after: sentinel, verbose: false }
+      end
+
+
+      case type
+      when :index
+        api = %(
+    #{routing_code}_index: ->
+      new App.Controllers.#{modul}.#{routing_code.camelize}(action: 'index')
+)
+      when :new
+        api = %(
+    #{routing_code}_new: ->
+      new App.Controllers.#{modul}.#{routing_code.camelize}(action: 'new')
+)
+      when :show
+        api = %(
+    #{routing_code}_show: (id) ->
+      new App.Controllers.#{modul}.#{routing_code.camelize}(id: id, action: 'show')
+)
+      when :edit
+        api = %(
+    #{routing_code}_edit: (id) ->
+      new App.Controllers.#{modul}.#{routing_code.camelize}(id: id, action: 'edit')
+)
+      end
+
+      sentinel = /API \=\s*$/
+
+      in_root do
+        inject_into_file "#{backbone_path}/routes.js.coffee", "\n#{api}", { after: sentinel, verbose: false }
+      end
+    end
   end
 end
